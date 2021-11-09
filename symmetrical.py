c2554bb58306ce8c1402a9335eb32b28f5dd6ad9
alchy <alchy@users.noreply.github.com>
2021-11-09 20:59:04 +0100

    Update symmetrical.py

--- symmetrical.py
+++ symmetrical.py
@@ -1,15 +1,35 @@
+from cryptography.fernet import Fernet
+import hashlib
 import base64
 
-key = 'abcd0000000000000000000000000000'
+APP_SC = '12341234123412341234123412341234'
 
-key_b64 = base64.b64encode(key.encode("utf-8"))
 
-data = 'message...'
+def encrypt(data, sc):
 
-data_encoded = fernet.encrypt(data.encode())
+    try:
 
-print('[d] encoded: ' + str(data_encoded))
+        key = hashlib.md5(APP_SC.encode() + sc.encode()).hexdigest()
 
-data_decoded = fernet.decrypt(data_encoded).decode()
+        fernet = Fernet(base64.b64encode(key.encode("utf-8")))
 
-print('[d] decoded: ' + data_decoded)
+        return fernet.encrypt(data.encode()).decode()
+
+    except Exception as e:
+
+        return '[!] encrypt: ' + repr(e)
+
+
+def decrypt(data, sc):
+
+    try:
+
+        key = hashlib.md5(APP_SC.encode() + sc.encode()).hexdigest()
+
+        fernet = Fernet(base64.b64encode(key.encode("utf-8")))
+
+        return fernet.decrypt(data.encode()).decode()
+
+    except Exception as e:
+
+        return '[!] decrypt: ' + repr(e)
